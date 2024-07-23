122c92db27256a91c90efaefcd6f856b3e0d4c09
jhq0113 <jhq0113@163.com>
2024-07-23 11:09:05 +0800

    add BytesRecord

--- record.go
+++ record.go
@@ -63,3 +63,65 @@ func (r Record) ToBytes(key string) []byte {
 	value, _ := r[key]
 	return convert.String2Bytes(value)
 }
+
+type BytesRecord map[string][]byte
+
+func (br BytesRecord) Exists(key string) bool {
+	_, exists := br[key]
+	return exists
+}
+
+func (br BytesRecord) String(key string) string {
+	value, _ := br[key]
+	return convert.Bytes2String(value)
+}
+
+func (br BytesRecord) ToBool(key string) bool {
+	value, _ := br[key]
+	return convert.Bytes2Bool(value)
+}
+
+func (br BytesRecord) ToInt(key string) int {
+	value, _ := br[key]
+	return int(convert.Bytes2Int64(value))
+}
+
+func (br BytesRecord) ToUint32(key string) uint32 {
+	value, _ := br[key]
+	return convert.Bytes2Uint32(value)
+}
+
+func (br BytesRecord) ToInt32(key string) int32 {
+	value, _ := br[key]
+	return convert.Bytes2Int32(value)
+}
+
+func (br BytesRecord) ToUint64(key string) uint64 {
+	value, _ := br[key]
+	return convert.Bytes2Uint64(value)
+}
+
+func (br BytesRecord) ToInt64(key string) int64 {
+	value, _ := br[key]
+	return convert.Bytes2Int64(value)
+}
+
+func (br BytesRecord) ToUint8(key string) uint8 {
+	value, _ := br[key]
+	return convert.Bytes2Uint8(value)
+}
+
+func (br BytesRecord) ToInt8(key string) int8 {
+	value, _ := br[key]
+	return convert.Bytes2Int8(value)
+}
+
+func (br BytesRecord) ToFloat64(key string) float64 {
+	value, _ := br[key]
+	return convert.Bytes2Float64(value)
+}
+
+func (br BytesRecord) Bytes(key string) []byte {
+	value, _ := br[key]
+	return value
+}
--- scan.go
+++ scan.go
@@ -42,3 +42,40 @@ func Scan(rows *sql.Rows, err error) ([]Record, error) {
 
 	return records, nil
 }
+
+func ScanBytesRecords(rows *sql.Rows, err error) ([]BytesRecord, error) {
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	fields, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		values  = make([]any, len(fields), len(fields))
+		records = make([]BytesRecord, 0, 8)
+	)
+
+	for index, _ := range fields {
+		values[index] = &[]byte{}
+	}
+
+	for rows.Next() {
+		err = rows.Scan(values...)
+		if err != nil {
+			return nil, err
+		}
+
+		record := make(BytesRecord, len(fields))
+		for index, field := range fields {
+			record[field] = *values[index].(*[]byte)
+		}
+		records = append(records, record)
+	}
+
+	return records, nil
+}
